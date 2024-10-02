5965d907453f81c4383819f03a1c4c0ab5eaf17a
Marouane LASMAK <lasmak2002@gmail.com>
2024-10-02 18:35:24 +0100

    finalizing

--- order-service/src/main/java/com/ecom/order_service/kafka/OrderConsumer.java
+++ order-service/src/main/java/com/ecom/order_service/kafka/OrderConsumer.java
@@ -2,26 +2,25 @@ package com.ecom.order_service.kafka;
 
 import com.ecom.order_service.dtos.PaymentCreated;
 import com.ecom.order_service.enums.OrderStatus;
-import com.ecom.order_service.repositories.OrderRepository;
+import com.ecom.order_service.exceptions.OrderNotFoundException;
+import com.ecom.order_service.services.OrderService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 @Slf4j
 @AllArgsConstructor
 public class OrderConsumer {
-    private final OrderRepository orderRepository;
+    private final OrderService orderService;
 
     @KafkaListener(topics = "${consumer-topic}", groupId = "${group-id}")
     public void consumeOrderCreated(PaymentCreated paymentCreated) {
-        orderRepository.findById(paymentCreated.getOrderId()).ifPresent(order -> {
-            order.setStatus(OrderStatus.COMPLETED);
-            orderRepository.save(order);
-            System.out.println("Order "+ paymentCreated.getOrderId()+" Completed.");
-        });
+        try {
+            orderService.changeOrderStatus(paymentCreated.getOrderId(), OrderStatus.COMPLETED);
+        } catch (OrderNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
